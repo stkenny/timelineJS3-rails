5a371024dada41e82127c828343a513558d6ee4e
Leonardo Diez <leiodd@gmail.com>
2016-07-12 16:13:14 +0200

    Path fix.

--- lib/timelineJS3/rails/engine.rb
+++ lib/timelineJS3/rails/engine.rb
@@ -2,7 +2,7 @@ module TimelineJS3
   module Rails
     class Engine < ::Rails::Engine
       initializer 'timelineJS3.assets.precompile' do |app|
-        %w(stylesheets javascripts icons locale).each do |sub|
+        %w(stylesheets javascripts icons).each do |sub|
           app.config.assets.paths << root.join('assets', sub).to_s
         end
 
