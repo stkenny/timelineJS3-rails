6af16ef13e8521d841082a9dae1d59a2f9317fe8
Leonardo Diez <leiodd@gmail.com>
2016-07-12 16:41:48 +0200

    Trying to fix development and production assets.

--- lib/timelineJS3/rails/engine.rb
+++ lib/timelineJS3/rails/engine.rb
@@ -2,8 +2,8 @@ module TimelineJS3
   module Rails
     class Engine < ::Rails::Engine
       initializer 'timelineJS3.assets.precompile' do |app|
-        %w(stylesheets javascripts).each do |sub|
-          app.config.assets.paths << root.join('assets', sub, 'timelineJS3').to_s
+        %w(stylesheets javascripts icons).each do |sub|
+          app.config.assets.paths << root.join('assets', sub).to_s
         end
 
         # sprockets-rails 3 tracks down the calls to `font_path` and `image_path`
