8972bba01271a170e742f934a1a53fe4a2374394
Leonardo Diez <leiodd@gmail.com>
2016-07-12 17:47:52 +0200

    Trying to fix timelineJS3 assets.

--- lib/timelineJS3/rails/engine.rb
+++ lib/timelineJS3/rails/engine.rb
@@ -1,17 +1,6 @@
 module TimelineJS3
   module Rails
     class Engine < ::Rails::Engine
-      initializer 'timelineJS3.assets.precompile' do |app|
-        %w(stylesheets javascripts timelineJS3/locale timelineJS3/icons).each do |sub|
-          app.config.assets.paths << root.join('assets', sub).to_s
-        end
-
-        # sprockets-rails 3 tracks down the calls to `font_path` and `image_path`
-        # and automatically precompiles the referenced assets.
-        unless Sprockets::Rails::VERSION.split('.', 2)[0].to_i >= 3
-          app.config.assets.precompile << %r(tl-icons\.(?:eot|svg|ttf|woff2?)$)
-        end
-      end
     end
   end
 end
