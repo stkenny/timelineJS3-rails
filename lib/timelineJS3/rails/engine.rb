fbcb884fcc6d80f97fee96eb3a48593f4024e3c7
Stuart Kenny <kennystu@gmail.com>
2025-10-15 16:01:23 +0100

    Regex not accepted

--- lib/timelineJS3/rails/engine.rb
+++ lib/timelineJS3/rails/engine.rb
@@ -4,7 +4,7 @@ module TimelineJS3
       config.assets.paths << config.root.join('vendor', 'assets', 'javascripts')
       config.assets.paths << config.root.join('vendor', 'assets', 'stylesheets')
       config.assets.paths << config.root.join('vendor', 'assets', 'fonts')
-      config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+      config.assets.precompile += [ "*.svg", "*.eot", "*.woff", "*.ttf" ]
     end
   end
 end
